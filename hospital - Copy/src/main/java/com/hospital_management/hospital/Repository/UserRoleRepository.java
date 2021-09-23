640397f70fc43cbd73a1eaa3cdd69dbf4672682b
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-09-23 08:52:12 +0530

    Delete hospital - Copy directory

--- hospital - Copy/src/main/java/com/hospital_management/hospital/Repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_management.hospital.Repository;
-
-import com.hospital_management.hospital.Entity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
-}
