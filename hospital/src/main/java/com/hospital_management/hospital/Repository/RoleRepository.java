33c150a18bf2ad3370c1a6e81f2f722544941d83
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-09-29 18:39:10 +0530

    Delete hospital directory

--- hospital/src/main/java/com/hospital_management/hospital/Repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_management.hospital.Repository;
-
-import com.hospital_management.hospital.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
