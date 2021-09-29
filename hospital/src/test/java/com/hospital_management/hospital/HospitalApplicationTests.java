e36bc00e7528756e7e344fe3d09ca6790211a626
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-09-29 11:49:22 +0530

    Add files via upload

---
+++ hospital/src/test/java/com/hospital_management/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_management.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
